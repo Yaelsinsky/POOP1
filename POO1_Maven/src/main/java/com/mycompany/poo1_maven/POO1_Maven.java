a140700d41b10d86fa5f6a6fd4f7d7c4ea9cf51d
Yaelsinsky <112461512+Yaelsinsky@users.noreply.github.com>
2023-10-12 20:47:17 -0600

    Add files via upload

---
+++ POO1_Maven/src/main/java/com/mycompany/poo1_maven/POO1_Maven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poo1_maven;
+
+/**
+ *
+ * @author Yael Eduardo Camarena Arevalo
+ */
+public class POO1_Maven {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo desde POO grupo 8 desde Maven");
+    }
+}
